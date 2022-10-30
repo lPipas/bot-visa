71c059a1259761e5aea7317301b44224da6590bd
Damian Mas <damianrmas@hotmail.com>
2022-10-30 20:23:01 -0300

    updates

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ function main() {
             await page.goto('https://ais.usvisa-info.com/es-ar/niv/schedule/43886158/continue');
             await delay(2000);
             let res = await page.evaluate(() => {
-                window.scroll(0, 500);
+                // PARA LA NOTEBOOK    window.scroll(0, 500);
                 if (document.querySelector('#consulate_date_time_not_available') === null) {
                     return true
                 } else {
@@ -59,39 +59,33 @@ function main() {
             });
             if (res) {
                 await delay(2000);
-                try {
-                    let res1 = await page.evaluate(() => {
-                        let h1 = document.querySelector('body > center:nth-child(1) > h1');
-                        if (h1.innerHTML !== '429 Too Many Requests') {
+                let res1 = await page.evaluate(() => {
+                    let h1 = document.querySelector('body > center:nth-child(1) > h1');
+                    if (h1.innerHTML !== '429 Too Many Requests') {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                });
+                if (res1) {
+                    let res2 = await page.evaluate(() => {
+                        let sign_in = document.querySelector('#flash_messages > div');
+                        if (sign_in.innerHTML !== '\nYou need to sign in or sign up before continuing.\n') {
                             return true;
                         } else {
                             return false;
                         }
                     });
-                    if (res1) {
-                        let res2 = await page.evaluate(() => {
-                            let sign_in = document.querySelector('#flash_messages > div');
-                            if (sign_in.innerHTML !== '\nYou need to sign in or sign up before continuing.\n') {
-                                return true;
-                            } else {
-                                return false;
-                            }
-                        });
-                        if (res2) {
-                            console.log('Turno');
-                            await client.sendMessage(numero, 'Hay turno disponible bro');
-                            await page.screenshot({ path: `${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}` });
-                        } else {
-                            console.log('Sign In is required');
-                            await browser.close();
-                        }
+                    if (res2) {
+                        console.log('Turno');
+                        await client.sendMessage(numero, 'HAY TURNO!!! :D');
+                        await page.screenshot({ path: `${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}` });
                     } else {
-                        console.log('Too Many Request');
+                        console.log('Sign In is required');
                         await browser.close();
                     }
-                } catch (error) {
-                    console.log(error);
-                    await page.screenshot({ path: `errores/${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}` });
+                } else {
+                    console.log('Too Many Request');
                     await browser.close();
                 }
             } else {
